3aa500a7f09082ccb47361c52df83feb66343eaa
dujun <jun.du@intel.com>
2022-11-22 02:49:36 +0000

    fix script

--- inference.py
+++ inference.py
@@ -340,7 +340,7 @@ def main():
 
     with torch.no_grad():
         datatype = torch.float16 if args.precision == "float16" else torch.bfloat16 if args.precision == "bfloat16" else torch.float
-        malconv = torch.xpu.optimize(model=malconv, dtype=datatype
+        malconv = torch.xpu.optimize(model=malconv, dtype=datatype)
         if args.precision == "float16" and args.device == "cuda":
             print("---- Use autocast fp16 cuda")
             with torch.cuda.amp.autocast(enabled=True, dtype=torch.float16):
